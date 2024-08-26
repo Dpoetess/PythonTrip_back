a65d311ff068155b47ce66b42758a9152f29beaa
Lynn Poh <pohwlynn@gmail.com>
2024-08-26 17:18:36 +0200

    created UserLoginView

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import RegisterView
+from .views import RegisterView, UserLoginView
 
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register')
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login')
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
 from rest_framework import generics, permissions, views, status
-from django.contrib.auth.models import User
-from .serializer import RegisterSerializer
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from .serializer import RegisterSerializer
+
 
 
 
@@ -10,6 +13,21 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [permissions.AllowAny]
+
+class UserLoginView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        if not email or not password:
+            return Response({'Error': 'Email and password are required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = authenticate(request, username=email, password=password)
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
 
 """class LogoutView(views.APIView):
