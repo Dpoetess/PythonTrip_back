8f25de74c83ad1588fe9b8fe52011d7bf41e1981
helopgom <helena.lopez.gomez@gmail.com>
2024-08-28 11:00:07 +0200

    Work in progress

---
+++ categories/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Category(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
