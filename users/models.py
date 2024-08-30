a101b3b056785a0eeef743efa87bf9a0eea278ca
helopgom <helena.lopez.gomez@gmail.com>
2024-08-30 19:32:14 +0200

    Passed user test

--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-"""class User(models.Model):
+class User(models.Model):
     first_name = models.CharField(max_length=80, null=False)
     last_name = models.CharField(max_length=80, null=False)
     user_name = models.CharField(max_length=100, unique=True)
@@ -12,4 +12,3 @@ from django.db import models
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.user_name})"
-"""
